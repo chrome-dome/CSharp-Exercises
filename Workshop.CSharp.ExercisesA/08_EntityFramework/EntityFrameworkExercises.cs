843195877fa58b1a81f637e409e8889c6db010ce
chrome-dome <chrome-domepl@protonmail.com>
2023-01-31 12:39:15 +0100

    DB context

--- Workshop.CSharp.ExercisesA/08_EntityFramework/EntityFrameworkExercises.cs
+++ Workshop.CSharp.ExercisesA/08_EntityFramework/EntityFrameworkExercises.cs
@@ -21,5 +21,51 @@ namespace Workshop.CSharp.EntityFramework.ExercisesB
         /// </summary>
         [TestMethod]
         public void DatabaseFromModel()  {  }
+
+        public class MyModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class MyDbContext : DbContext
+        {
+            public DbSet<MyModel> MyModels { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+            }
+
+            internal void SaveChanges()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class DatabaseFromModelExample
+        {
+            public static void Main()
+            {
+                using (var context = new MyDbContext())
+                {
+                    context.Database.EnsureCreated();
+
+                    // Add data
+                    context.MyModels.Add(new MyModel { Name = "Model 1", Description = "Description 1" });
+                    context.SaveChanges();
+
+                    // Modify data
+                    var model = context.MyModels.First();
+                    model.Name = "Modified Model 1";
+                    context.SaveChanges();
+
+                    // Remove data
+                    context.MyModels.Remove(model);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
